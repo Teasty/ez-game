99083d4fd0e786631ac4e1a3788e648cfcf595c3
Teasty <av-likhachev@yandex.ru>
2018-03-23 16:52:54 +0300

    Working rotation

--- Cube/Assets/Scripts/RotationScript.cs
+++ Cube/Assets/Scripts/RotationScript.cs
@@ -4,80 +4,102 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour {
 
-	public float speed;
+	public float speed = 200;
 	public SwipeController Controls;
 	float yRotation, xRotation, zRotation;
 	float piecerotation;
+	private GameObject endRotation;
 
-	// Use this for initialization
-	void Start () {
-		
-
+	void Start(){
+		endRotation = new GameObject();
 	}
-	
+
+//	void Update () {
+//		if( Input.GetKeyDown( KeyCode.RightArrow ) ){
+//			endRotation.transform.Rotate(Vector3.forward, 90, Space.World);
+//		}
+//		if( Input.GetKeyDown( KeyCode.LeftArrow ) ){
+//			endRotation.transform.Rotate(Vector3.forward, -90, Space.World);
+//		}
+//
+//
+//		if( Input.GetKeyDown( "a" ) ){
+//			endRotation.transform.Rotate(Vector3.up, 90, Space.World);
+//		}
+//		if( Input.GetKeyDown( "d" ) ){
+//			endRotation.transform.Rotate(Vector3.up, -90, Space.World);
+//		}
+//
+//
+//		if( Input.GetKeyDown( "w" ) ){
+//			endRotation.transform.Rotate(Vector3.left, 90, Space.World);
+//		}
+//		if( Input.GetKeyDown( "s") ){
+//			endRotation.transform.Rotate(Vector3.left, -90, Space.World);
+//		}
+//
+//
+//	}
 	// Update is called once per frame
 	void Update () {
 
 		if (Controls.SwipeLeft) {
-			Rotatey (90);
+			endRotation.transform.Rotate (Vector3.up, 90, Space.World);
 		}
 		if (Controls.SwipeRight) {
-			Rotatey (-90);
+			endRotation.transform.Rotate (Vector3.up, -90, Space.World);
 		}
 
 
 		if (Controls.SwipeUp) {
-			if (yRotation == 0)
-				Rotatex (90);
-			else if (yRotation == 90|| yRotation == -270)
-				Rotatez (90);
-			else if (yRotation == -90 || yRotation == 270)
-				Rotatez (-90);
-			else if (yRotation == 180 || yRotation == -180)
-				Rotatex (-90);
+//			if (yRotation == 0)
+			endRotation.transform.Rotate (Vector3.left, -90, Space.World);
+//			else if (yRotation == 90|| yRotation == -270)
+//				Rotatez (90);
+//			else if (yRotation == -90 || yRotation == 270)
+//				Rotatez (-90);
+//			else if (yRotation == 180 || yRotation == -180)
+//				Rotatex (-90);
 		}
 
 		if (Controls.SwipeDown) {
-			if (yRotation == 0)
-				Rotatex (-90);
-			else if (yRotation == 90|| yRotation == -270)
-				Rotatez (-90);
-			else if (yRotation == -90 || yRotation == 270)
-				Rotatez (90);
-			else if (yRotation == 180 || yRotation == -180)
-				Rotatex (90);
+//			if (yRotation == 0)
+			endRotation.transform.Rotate (Vector3.left, 90, Space.World);
+//			else if (yRotation == 90|| yRotation == -270)
+//				Rotatez (-90);
+//			else if (yRotation == -90 || yRotation == 270)
+//				Rotatez (90);
+//			else if (yRotation == 180 || yRotation == -180)
+//				Rotatex (90);
 		}
-
-		if (true) {
-			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (xRotation, yRotation, zRotation), speed);
-			}
+		this.transform.rotation = Quaternion.Lerp (this.transform.rotation, endRotation.transform.rotation, speed);
 	}
 
 		
 
-	public void Rotatey(float e){
-
-		if (yRotation == 360|| yRotation == -360) {
-			yRotation = 0;
-		}
-
-		yRotation += e;
-		Debug.Log (xRotation.ToString() + " " + yRotation.ToString() + " " + zRotation.ToString());
-	}
-	public void Rotatex(float e){
-
-		if (xRotation == 360 || xRotation == -360) {
-			xRotation = 0;
-		}
-
-		xRotation += e;
-	}
-	public void Rotatez(float e){
-
-		if (zRotation == 360 || zRotation == -360) {
-			zRotation = 0;
-		}
-
-		zRotation += e;
-	}
+//	public void Rotatey(float e){
+//
+//		if (yRotation == 360|| yRotation == -360) {
+//			yRotation = 0;
+//		}
+//
+//		yRotation = transform.rotation.eulerAngles.y + e;
+//		Debug.Log (xRotation.ToString() + " " + yRotation.ToString() + " " + zRotation.ToString());
+//	}
+//	public void Rotatex(float e){
+//
+//		if (xRotation == 360 || xRotation == -360) {
+//			xRotation = 0;
+//		}
+//
+//		xRotation = transform.rotation.eulerAngles.x + e;
+//	}
+//	public void Rotatez(float e){
+//
+//		if (zRotation == 360 || zRotation == -360) {
+//			zRotation = 0;
+//		}
+//
+//		zRotation = transform.rotation.eulerAngles.z + e;
+//	}
 }
