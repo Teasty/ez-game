7df0e73456fc8c42b6f717e72d0e12e7e28fbf53
Tenlern <illyash20009@yadnex.ru>
2018-04-03 20:19:14 +0300

    +Pause

---
+++ Cube/Assets/Scripts/Pause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public Transform canvas;
+    //public SwipeController Controls;
+    public GameObject cube;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            setPause();
+        }
+	}
+
+    public void setPause(){
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            Time.timeScale = 0;
+            cube.GetComponent<SwipeController>().enabled = false;
+            canvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            cube.GetComponent<SwipeController>().enabled = true;
+            Time.timeScale = 1;
+        } 
+    }
+}
